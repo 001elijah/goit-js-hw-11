27b74d7b8b8492f951d76ff41241914367938479
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-03-07 21:07:33 +0200

    update

---
+++ src/js/gallery.js
@@ -0,0 +1,7 @@
+'use strict';
+// https://pixabay.com/api/
+// import { pixabayAPI } from ./pixabay-api;
+
+// const searchFormEl = document.querySelector();
+// const galleryListEl = document.querySelector();
+// const loadMoreBtnEl = document.querySelector();
---
+++ src/js/pixabay-api.js
@@ -0,0 +1,3 @@
+'use strict'
+
+// #API_KEY = 34207283-4bddbd3e33d2a5a82fc6e194a;
